c3edb220b14e9d251741f398a14f2e28e91d973e
Eleanor Hall <ruthhall97@gmail.com>
2020-11-11 02:28:59 -0800

    broken our homepage into small components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 
-import HomePage from './homepage'
+import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
-    <div >
+    <div>
      <HomePage/>
     </div>
   );
---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory';
+import './homepage.styles.scss';
+
+const Homepaege = () => (
+    <div className='homepage'>
+       <Directory/>
+    </div>
+);
+
+export default Homepaege;
