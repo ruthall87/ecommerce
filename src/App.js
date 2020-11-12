e199a039da685d03df5dc390a959254a007cc01a
Eleanor Hall <ruthhall97@gmail.com>
2020-11-11 20:05:23 -0800

    logo is not appending to page spent too much time on such a simple thing will come back it, on the brightside added links to homepage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import ShopPage from './pages/shop/shop';
 import HomePage from './pages/homepage/homepage';
-
+import Header from './components/header/header'
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch> 
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
